1568c96e67e4c4237691a590a079d7108bbe62ea
49923230 <49923230@gm.nfu.edu.tw>
2015-11-09 13:57:18 +0800

    49923230

--- pelican/publishconf.py
+++ pelican/publishconf.py
@@ -8,7 +8,7 @@ from __future__ import unicode_literals
 import os
 
 AUTHOR = 'KMOL'
-SITENAME = '2015FALL KMOL 課程'
+SITENAME = '2015FALL 49923230 Homework'
 SITEURL = ''
 
 PATH = 'content'
@@ -27,6 +27,7 @@ AUTHOR_FEED_RSS = None
 # Blogroll
 LINKS = (('2015課程網頁', 'http://wordpress-2015course.rhcloud.com/'),
          ('Python', 'http://python.org/'),
+         ('My github', 'https://github.com/49923230/homework/tree/gh-pages'),
          ('You can modify those links in your config file', '#'),)
 
 # Social widget
